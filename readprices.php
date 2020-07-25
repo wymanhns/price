595cb2d5a46349c05a7df65f04d668ae0b02eadf
wymanhns <wymanhns@gmail.com>
2020-07-25 10:28:49 +0800

    first commit

--- readprices.php
+++ readprices.php
@@ -1,27 +1,38 @@
 <?php
 
 //  url  
-$cpuurl="https://www.price.com.hk/category.php?c=100014&sort=2";
-$ramurl="https://www.price.com.hk/category.php?c=100027&gp=20&sort=2";
+$cpu=array("cpu","https://www.price.com.hk/category.php?c=100014&sort=2");
+$ramurl=array("cpu","https://www.price.com.hk/category.php?c=100027&gp=20&sort=2");
 
 /*   test url */
-//$cpuurl="http://127.0.0.1/w1/cpu.html?"; 
+#$cpu=array("cpu","http://127.0.0.1/w1/cpu.html?"); 
 //$ramurl="http://127.0.0.1/w1/ram.html?"; 
 
 
-ReadCpu($cpuurl);
+ReadPrices($cpu);
 
 
 
-function ReadCpu($url) {
+function ReadPrices($dataname) {
+
+    $NameData=$dataname[0];
+    $url=$dataname[1];
 
     $rData=array();
     $response = getHTTPS($url);
     $result1=strstr($response,"共");
     $result2=strstr($result1,"種項目" ,1);
     $total=substr($result2,3);
-
     $pagecount=15;
+
+    //for test
+        if (stripos($url,'price.com.hk')){
+            $pagecount=15;
+        } else {
+            $pagecount=$total;
+        }
+    //
+    
     $pages=$total/$pagecount;
     $lastpagecount=$total%$pagecount;
     if ($lastpagecount==0){ $lastpagecount=$pagecount; }
@@ -37,182 +48,176 @@ function ReadCpu($url) {
         echo $url1. "<br>";
         $resultx = getHTTPS($url1);
         $writeresult=$writeresult . "<br>" . $resultx;
-        
-        echo "******************************* Page ".$i." *************************************<br>";
-        $pagecount=15;
 
-            
-        $result2=strstr($resultx,"li id="); //</li>
-        #$result2=strstr($result1,'</li>',1);
-        #$resultx=strstr($result1,'</li>');
-        #$result4=strstr($result3,'li id="footer-wechat"',1);
-        #$result2=$resultx;
-        
-        #echo $result2;
+        print_r ("<<<******* Page ".$i." *******>>>");
 
-        for ($y=1; $y<=15; $y++)
+        
+        $result2=strstr($resultx,'<li id="track_'); //</li>
+        #$pagecount=2;
+
+        for ($y=1; $y<=$pagecount; $y++)
         {
 
             $xy = $y + (($i - 1) * 15);
             #echo $result2;
 
-            echo "******************************** " . $xy . " / ". $total . "**********************************<br>";
+            print_r ("<br>********** " . $xy . " / ". $total . " **********<br>");
+
+            $itemopen=stristr($result2,'<!--item-->');
+            $result2=substr($itemopen,11);
+            $itemclose=stristr($result2,'<!-- product price -->',1);
+            
+            #print_r ($itemclose);  // 每一件物件 HTML
 
             // cpuid
-            $resulta=strstr($result2,'data-add-compare="'); 
-            $resultb=strstr($resulta,'data-add-compare-cid',1);
-            preg_match_all('/\d+/',$resultb,$core);;  //只取數字
-            $cpuid=$core[0][0];
+                $resulta=strstr($itemclose,'data-add-compare="'); 
+                $resultb=strstr($resulta,'data-add-compare-cid',1);
+                preg_match_all(CheckDot($resultb),$resultb,$core);
+                $cpuid=$core[0][0];
             //
             // cpuname
-            $resulta=strstr($result2,'data-add-compare-name="'); 
-            $resultb=strstr($resulta,'/>',1);
-            $cpuname=substr($resultb,23);
-            $cpuname=strstr($cpuname,'"',1);
-            //echo $cpuname . "///////////";
+                $resulta=strstr($itemclose,'data-add-compare-name="'); 
+                $resultb=strstr($resulta,'/>',1);
+                $cpuname=substr($resultb,23);
+                $cpuname=strstr($cpuname,'"',1);
+            //
+            // cpufamily
+                $resulty=strstr($itemclose,'"product-caption">('); 
+                $resultz=strstr($resulty,')</span></div>',1);
+                $resultb=substr($resultz,19);
+                $cpufamily=strstr($resultb,',',1);
+                
+                if (stripos($resultb,',')){$cpufamily=strstr($resultb,',',1); } else { $cpufamily=0;}
+
+                $resulty=strstr($cpuname,' '); 
+                $resulty=substr($resulty,1);
+                if (stripos($resulty,'-')){$cpufamily=strstr($resulty,'-',1); } else { $cpufamily=strstr($resulty,' ',5);}
             //
             // brand 
-            if (preg_match("/intel/i", $cpuname)) {
-                $brand="Intel";
-            } elseif (preg_match("/amd/i", $cpuname)) {
-                $brand="Amd";
-            } elseif (preg_match("/via/i", $cpuname)) {
-                $brand="VIA";
-            } else {
-                $brand="未知品牌。";
-            }
-            //    
+                if (preg_match("/intel/i", $cpuname)) {
+                    $brand="Intel";
+                } elseif (preg_match("/amd/i", $cpuname)) {
+                    $brand="Amd";
+                } elseif (preg_match("/via/i", $cpuname)) {
+                    $brand="VIA";
+                } else {
+                    $brand="未知品牌。";
+                }
+            //  
             // cpupic
-                $resulta=strstr($result2,'/space'); 
+                $resulta=strstr($itemclose,'/space/product/'); 
                 $resultb=strstr($resulta,'"/></a>',1);
                 $cpupic=substr($resultb,1);
             //
             // hot         
-            $resulty=strstr($resulta,'<!-- product name -->',1);
-            $resultz=strstr($resulty,'hot_20'); 
-            $resulty=strstr($resultz,'人氣項目',1);
-            #print_r (strlen($resulty)); 
-            $hot=0;
-            if (strlen($resulty) > 0){
-                $hot=1;
-                echo "*** Hot! ***";
-            }
+                $resulty=strstr($itemclose,'<!-- product name -->',1);
+                $resultz=strstr($resulty,'hot_20'); 
+                $resulty=strstr($resultz,'人氣項目',1);
+                $hot=0;
+                if (strlen($resulty) > 0){
+                    $hot=1;
+                    echo "*** Hot! ***";
+                }
             // 
-            // cpu family cpucore cputhreads mtfrequency
-            if ($brand == "Intel"){
-                $resulta=strstr($result2,'<!-- product name -->'); 
-                $resultb=strstr($resulta,'<!-- product rating -->',1);
-                if (strlen($resultb) > 500 ) {
-                    $resulty=strstr($resultb,'"product-caption">('); 
-                    $resultz=strstr($resulty,')</span></div>',1);
-                    $resultb=substr($resultz,19);
-                    $cpufamily=strstr($resultb,',',1);
-                    $resulty=strstr($resultz,'('); 
-                    $resultz=stristr($resulty,'core',1);
-                    $cpucore=substr($resultz,1);
-                    preg_match_all('/\d+/',$resultz,$core);;  //只取數字
-                    $cpucore=$core[0][0];
-                    $resulty=stristr($resultb,'core'); 
-                    $resultz=stristr($resulty,'threads',1);
-                    $cputhreads=substr($resultz,4);
-                    preg_match_all('/\d+/',$cputhreads,$threads);;  //只取數字
-                    $cputhreads=$threads[0][0];
-                    $resulta=strstr($result2,'外頻:');
-                    $resulta=strstr($resulta,'<span>'); 
-                    $resultb=strstr($resulta,'Hz',1);
-                    $cpumtfrequency=substr($resultb,6);
-                    #echo strlen($cpumtfrequency);  
-                } else {
-                    $cpufamily="";
-                    $cpucore="";
-                    $cputhreads="";
-                }
-            } else {
-                $resulta=strstr($result2,'<!-- product name -->'); 
-                $resultb=strstr($resulta,'<!-- product rating -->',1);
-                if (strlen($resultb) > 500 ) {
-                    $resulty=strstr($resultb,'"product-caption">('); 
-                    $resultz=strstr($resulty,')</span></div>',1);
-                    $resultb=substr($resultz,19);
-                    $cpufamily=0;
-                    $resulty=strstr($resultz,'('); 
-                    $resultz=stristr($resulty,'core',1);
-                    $cpucore=substr($resultz,1);
-                    preg_match_all('/\d+/',$resultz,$core);;  //只取數字
-                    $cpucore=$core[0][0];
-                    $resulty=stristr($resultb,'core'); 
-                    $resultz=stristr($resulty,'thread',1);
-                    $cputhreads=substr($resultz,4);
-                    preg_match_all('/\d+/',$cputhreads,$threads);;  //只取數字
-                    $cputhreads=$threads[0][0];
-                    $cpumtfrequency="";
-                } else {
-                    $cpufamily="";
-                    $cpucore="";
-                    $cputhreads="";
-                }
-            }
+            // cpunew
+                $cpunew=$xy;
             //
-
-            // cpufrequency 
+            // cpucore
+                $resulty=strstr($itemclose,'product-caption">('); 
+                $resultz=stristr($resulty,'core',1);
+                $cpucore=substr($resultz,18);
+                if (strlen($cpucore) > 3){$cpucore=substr($cpucore,strlen($cpucore) -3);}
+                #print_r ($cpucore."**<br>");
+                preg_match_all(CheckDot($cpucore),$cpucore,$core);
+                $cpucore=$core[0][0];
+            //
+            // cputhreads 
+                $resulty=stristr($resulty,'core'); 
+                $resultz=stristr($resulty,'threads',1);
+                $cputhreads=substr($resultz,4);
+                preg_match_all(CheckDot($cputhreads),$cputhreads,$threads); 
+                $cputhreads=$threads[0][0];
+            //
+            // cpufrequency cpumtfrequency 
                 $resulta=strstr($result2,'時脈'); 
                 $resulta=strstr($resulta,'<span>'); 
-                $resultb=strstr($resulta,'Hz',1);
-                $cpufrequency=substr($resultb,6);
-                #echo strlen($cpufrequency);
-                #echo $cpufrequency . "///////////";  
+                $resulta=strstr($resulta,'</span>',1);
+                $resulta=substr($resulta,6);
+                $Ghz=stripos($resulta,'g');
+                if (stripos($resulta,'/')){
+                    $resultb=strstr($resulta,'/',1);
+                    preg_match_all(CheckDot($resultb),$resultb,$frequency);  
+                    $resultc=strstr($resulta,'/');
+                    $cpufrequency=$frequency[0][0];
+                    preg_match_all(CheckDot($resultc),$resultc,$mtfrequency);  
+                    $cpumtfrequency=$mtfrequency[0][0];
+                } else {
+                    preg_match_all(CheckDot($resulta),$resulta,$frequency);  
+                    $cpufrequency=$frequency[0][0];
+                    $cpumtfrequency=0;
+                }
+            // cpumtfrequency by "外頻:"
+                if (stripos($itemclose,'外頻:')){
+                    $resulta=strstr($itemclose,'外頻:');
+                    $resulta=strstr($resulta,'<span>'); 
+                    $resultb=strstr($resulta,'Hz',1);
+                    preg_match_all(CheckDot($resultb),$resultb,$mtfrequency);  
+                    $cpumtfrequency=$mtfrequency[0][0];
+                    #$cpumtfrequency=substr($resultb,6);
+                    #echo strlen($cpumtfrequency); 
+                }
+                if ($Ghz>0){$cpufrequency=$cpufrequency*1000; $cpumtfrequency=$cpumtfrequency*1000; } //Ghz
             //
             // cpusocket
-                $resulta=strstr($result2,'Socket:'); 
+                $resulta=strstr($itemclose,'Socket:'); 
                 $resulta=strstr($resulta,'<span>');
                 $resultb=strstr($resulta,'</span></td>',1);
                 $cpusocket=substr($resultb,6);
             //
             // cpucache
-                $resulta=strstr($result2,'Cache:'); 
-                $resulta=strstr($resulta,'<span>');
-                $resultb=strstr($resulta,'</span></td>',1);
-                $cpucache=substr($resultb,6);
+                $resulta=stristr($itemclose,'Cache:'); 
+                $resultb=stristr($resulta,'<span>');
+                $resultb=strstr($resultb,'</span>',1);
+                $resultb=substr($resultb,6);
+                $kmcheck=stripos($resultb,'k');
+                #print_r ($kmcheck."*Cache** ".$resultb);
+                
+                if ($kmcheck>0) {$resultb=stristr($resultb,'k',1); $cpucache=$resultb."KB"; } else {$resultb=stristr($resultb,'m',1); $cpucache=$resultb."MB"; }
+                #print_r ("*Cache** ".$resultb);
+                #$cpucache=$resultb;
                 #echo strlen($cpucache);
             //
             // cpuminprice cpumaxprice    text-price-number    行貨
-                $resulta=strstr($result2,'text-price-number" data-price="'); 
-                $resultb=strstr($resulta,'</span>',1);
-                $stoppos=strpos(substr($resultb,31),'.');
-                #echo "//".$resulta ."//";
-                $cpuminprice=substr($resultb,31,$stoppos);
+                $resulta=strstr($itemclose,'text-price-number" data-price="'); 
+                $resultb=strstr($resulta,'">',1);
+                //$stoppos=strpos(substr($resultb,31),'.');
+                $resultb=substr($resultb,31);
+                preg_match_all(CheckDot($resultb),$resultb,$cpuminprice);  
+                $cpuminprice=$cpuminprice[0][0];
                 #echo "///".strlen($cpuminprice)."///";
                 $resulta=substr($resulta,31);
                 $resulta=strstr($resulta,'text-price-number" data-price="'); 
                 $resultb=strstr($resulta,'</span>',1);
-                $stoppos=strpos(substr($resultb,31),'.');
-                $cpumaxprice=substr($resultb,31,$stoppos);
+                $resultb=substr($resultb,31);
+                preg_match_all(CheckDot($resultb),$resultb,$cpumaxprice);  
+                $cpumaxprice=$cpumaxprice[0][0];
+                if (!$cpumaxprice){ $cpumaxprice=$cpuminprice;}
                 #echo "//".substr($resulta,31) ."//";
                 #echo strlen($cpumaxprice);  
             //
-            // cpunew
-                $cpunew=$xy;
-            //
-            #echo "Strlen : ". strlen($resultb) . "<br>*" .$resultb . "*<br>" ;
-            echo "CPUID:" . $cpuid . " CPU BRAND: " . $brand . " CPU NAME: " . $cpuname . " FAMILY: " . $cpufamily . " CORE: " .$cpucore. " THREADS: " .$cputhreads. " FREQUENCY: " . $cpufrequency."Hz MAX FREQUENCY: " .$cpumtfrequency . "Hz SOCKET: " .$cpusocket. " CACHE: " .$cpucache ."  <br>";
+
+            echo "CPUID:" . $cpuid . " CPU BRAND:" . $brand . " CPU NAME:" . $cpuname . " FAMILY:" . $cpufamily . " CORE:" .$cpucore. " THREADS:" .$cputhreads. " FREQUENCY:" . $cpufrequency."MHz MAX FREQUENCY:" .$cpumtfrequency . "MHz SOCKET:" .$cpusocket. " CACHE:" .$cpucache ."  <br>";
             echo "IMG: <img src='https://www.price.com.hk/" . $cpupic . "'/>" . "   <br>";
             echo " MINPRICE: " . $cpuminprice . " MAXPRICE: " . $cpumaxprice . "   <br>";
-            
-            
-            
+            #print_r ($cpucache);
             array_push($rData,array($cpuid,$brand,$cpuname,$cpufamily,"https://www.price.com.hk/" . $cpupic ,$cpusocket,$cpucore,$cputhreads,$cpufrequency,$cpumtfrequency,$cpucache,$cpuminprice,$cpumaxprice,$cpunew,$hot));
-            $result1=strstr($result2,"比較報價");
-            $result2=substr($result1,17);
-            #$result2=$resultx;
-            #echo $resultx;
-        }
 
+        }
+        #print_r ($itemopen);
         #echo $result2;
         $jsonx = json_encode($rData);
-        print_r ($jsonx);
+        #print_r ($jsonx);
         file_put_contents('cpu.json', $jsonx);
-
-        
 
         
 
@@ -220,16 +225,22 @@ function ReadCpu($url) {
 
     
     $myfile = fopen( "cpu.html", "w") or die("Unable to open file!");
-    #$txt = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' . "總數 ".$total." 個 , 共有 ".$pages." 頁,尾頁 " .$lastpagecount." 個<br>". $writeresult;
+    //$txt = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' . "總數 ".$total." 個 , 共有 ".$pages." 頁,尾頁 " .$lastpagecount." 個<bar>". $writeresult;
     $txt = $writeresult;
     fwrite($myfile, $txt);
     fclose($myfile);
     
 
-}    
+}
 
+function CheckDot($vol) { //只數字 '/\d+/' 數字带小数点 C'/(\d+)\.(\d+)/is'
+    if (stripos($vol,'.')){
+        return '/(\d+)\.(\d+)/is';
+    } else {
+        return '/\d+/';
+    }
 
-
+}
 
 function getHTTPS($url) {
     $curl = curl_init(); 
